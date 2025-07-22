93e3cc8e13a205f57d1315bfaf71ee3f1486fbe4
Super User <wwyxhqc1@foxmail.com>
2025-07-22 12:10:25 +0800

    auth

---
+++ middlewares/auth.go
@@ -0,0 +1,29 @@
+package middlewares
+
+import (
+	"net/http"
+	"strings"
+	"user-service/utils"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Authorization header required"})
+			return
+		}
+
+		tokenString := strings.Replace(authHeader, "Bearer ", "", 1)
+		userID, err := utils.ParseToken(tokenString)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			return
+		}
+
+		c.Set("userID", userID)
+		c.Next()
+	}
+}
