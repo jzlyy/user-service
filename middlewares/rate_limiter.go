693555f3ca2cb24f981b8456ebd6540aeba57dea
Fucheng Zhu <wwyxhqc1@foxmail.com>
2025-08-01 14:14:51 +0800

    rate_limiter

--- middlewares/rate_limiter.go
+++ middlewares/rate_limiter.go
@@ -1,1 +1,23 @@
 package middlewares
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"golang.org/x/time/rate"
+)
+
+func RateLimiter() gin.HandlerFunc {
+	// 100请求/秒，突发30请求
+	limiter := rate.NewLimiter(100, 30)
+
+	return func(c *gin.Context) {
+		if !limiter.Allow() {
+			c.AbortWithStatusJSON(http.StatusTooManyRequests, gin.H{
+				"error": "too many requests",
+			})
+			return
+		}
+		c.Next()
+	}
+}
