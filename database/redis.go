1967f7cd9d3c6c41c67df175e2ada49d44e9fea9
fucheng zhu <wwyxhqc1@foxmail.com>
2025-08-02 14:03:52 +0800

    redis

--- database/redis.go
+++ database/redis.go
@@ -1,1 +1,42 @@
 package database
+
+import (
+	"context"
+	"time"
+	"user-service/config"
+
+	"github.com/go-redis/redis/v8"
+)
+
+var RedisClient *redis.Client
+
+func InitRedis() error {
+	cfg := config.LoadConfig()
+
+	RedisClient = redis.NewClient(&redis.Options{
+		Addr:         cfg.RedisHost + ":" + cfg.RedisPort,
+		Password:     cfg.RedisPassword,
+		DB:           cfg.RedisDB,
+		DialTimeout:  5 * time.Second,
+		ReadTimeout:  3 * time.Second,
+		WriteTimeout: 3 * time.Second,
+		PoolSize:     20, // 连接池大小
+		MinIdleConns: 5,  // 最小空闲连接
+		IdleTimeout:  5 * time.Minute,
+	})
+
+	// 健康检查
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	if _, err := RedisClient.Ping(ctx).Result(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func CloseRedis() {
+	if RedisClient != nil {
+		_ = RedisClient.Close()
+	}
+}
