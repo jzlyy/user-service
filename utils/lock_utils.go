c9aa63dc4588bbb0b905a33b88f20b62b8e1971c
fucheng zhu <wwyxhqc1@foxmail.com>
2025-08-02 14:03:34 +0800

    lock_utils

--- utils/lock_utils.go
+++ utils/lock_utils.go
@@ -1,1 +1,21 @@
 package utils
+
+import (
+	"context"
+	"time"
+	"user-service/database"
+)
+
+const (
+	DefaultLockTimeout = 5 * time.Second
+)
+
+func AcquireLock(key string, timeout time.Duration) bool {
+	ctx := context.Background()
+	return database.RedisClient.SetNX(ctx, "lock:"+key, "1", timeout).Val()
+}
+
+func ReleaseLock(key string) {
+	ctx := context.Background()
+	database.RedisClient.Del(ctx, "lock:"+key)
+}
