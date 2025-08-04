20188754ff5d1acca13ce0b47fdec1d4fdb5c9e0
Fucheng Zhu <wwyxhqc1@foxmail.com>
2025-08-04 16:10:46 +0800

    email_utils

--- utils/email_utils.go
+++ utils/email_utils.go
@@ -1,1 +1,9 @@
 package utils
+
+import "regexp"
+
+// IsEmail 检查字符串是否是有效的电子邮件格式
+func IsEmail(str string) bool {
+	emailRegex := regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+	return emailRegex.MatchString(str)
+}
