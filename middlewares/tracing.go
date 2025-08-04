3d22e82261e89e53668b90b1d30d67aff917b099
Fucheng Zhu <wwyxhqc1@foxmail.com>
2025-08-04 14:28:14 +0800

    tracing

--- middlewares/tracing.go
+++ middlewares/tracing.go
@@ -1,1 +1,25 @@
 package middlewares
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/opentracing/opentracing-go"
+)
+
+func TracingMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		spanCtx, _ := opentracing.GlobalTracer().Extract(
+			opentracing.HTTPHeaders,
+			opentracing.HTTPHeadersCarrier(c.Request.Header),
+		)
+
+		span := opentracing.GlobalTracer().StartSpan(
+			c.FullPath(),
+			opentracing.ChildOf(spanCtx),
+		)
+		defer span.Finish()
+
+		ctx := opentracing.ContextWithSpan(c.Request.Context(), span)
+		c.Request = c.Request.WithContext(ctx)
+		c.Next()
+	}
+}
