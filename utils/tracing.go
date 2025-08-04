c031111053e10c28572006fd923a8e509508601c
Fucheng Zhu <wwyxhqc1@foxmail.com>
2025-08-04 14:28:48 +0800

    tracing

--- utils/tracing.go
+++ utils/tracing.go
@@ -1,1 +1,30 @@
 package utils
+
+import (
+	"github.com/opentracing/opentracing-go"
+	"github.com/uber/jaeger-client-go"
+	"github.com/uber/jaeger-client-go/config"
+)
+
+func InitTracing(serviceName string) (opentracing.Tracer, func(), error) {
+	cfg := config.Configuration{
+		ServiceName: serviceName,
+		Sampler: &config.SamplerConfig{
+			Type:  jaeger.SamplerTypeConst,
+			Param: 1,
+		},
+	}
+
+	tracer, closer, err := cfg.NewTracer()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	opentracing.SetGlobalTracer(tracer)
+	return tracer, func() {
+		err := closer.Close()
+		if err != nil {
+			return
+		}
+	}, nil
+}
