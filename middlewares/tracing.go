9b51515cb71b4a76ae191d200728e26ae93ad357
Fucheng Zhu <wwyxhqc1@foxmail.com>
2025-08-05 13:43:17 +0800

    tracing

--- middlewares/tracing.go
+++ middlewares/tracing.go
@@ -3,6 +3,8 @@ package middlewares
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/ext"
+	"net/http"
 )
 
 func TracingMiddleware() gin.HandlerFunc {
@@ -12,14 +14,25 @@ func TracingMiddleware() gin.HandlerFunc {
 			opentracing.HTTPHeadersCarrier(c.Request.Header),
 		)
 
-		span := opentracing.GlobalTracer().StartSpan(
+		span := opentracing.StartSpan(
 			c.FullPath(),
-			opentracing.ChildOf(spanCtx),
+			ext.RPCServerOption(spanCtx),
+			opentracing.Tag{Key: string(ext.Component), Value: "HTTP"},
+			opentracing.Tag{Key: "http.method", Value: c.Request.Method},
+			opentracing.Tag{Key: "http.url", Value: c.Request.URL.String()},
 		)
 		defer span.Finish()
 
+		// 注入到上下文
 		ctx := opentracing.ContextWithSpan(c.Request.Context(), span)
 		c.Request = c.Request.WithContext(ctx)
+
 		c.Next()
+
+		// 记录状态码
+		ext.HTTPStatusCode.Set(span, uint16(c.Writer.Status()))
+		if c.Writer.Status() >= http.StatusInternalServerError {
+			ext.Error.Set(span, true)
+		}
 	}
 }
