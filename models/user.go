8555121a9611b98980af76c55e5acd86b626d86f
Fucheng Zhu <wwyxhqc1@foxmail.com>
2025-08-06 10:27:46 +0800

    user

--- models/user.go
+++ models/user.go
@@ -16,3 +16,15 @@ type LoginRequest struct {
 	Identifier string `json:"identifier" binding:"required"` // 统一标识符字段
 	Password   string `json:"password" binding:"required,min=6"`
 }
+
+type ErrorResponse struct {
+	Error string `json:"error" example:"错误信息"`
+}
+
+type MessageResponse struct {
+	Message string `json:"message" example:"成功信息"`
+}
+
+type TokenResponse struct {
+	Token string `json:"token" example:"JWT令牌"`
+}
